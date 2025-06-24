72fb0eabc9416fa334f8bad88dd9e1bb6e0cf87b
mouhcineattif-in-ctech <amohcine@in-ctech.com>
2025-06-24 17:01:35 +0100

    feature/SCRUM-14-Ajout du foreign Key de la table compte_bancaire au table project (Migration)

---
+++ database/migrations/2025_06_24_155908_add_compte_bancaire_id_to_projects_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_compte_bancaire');
+            $table->foreign('id_compte_bancaire')->references('id')->on('comptes_bancaires');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
+    }
+};
